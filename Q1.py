f3ea8eba81d204a4516d2f7c2639c2e56bcf65bd
dtonda8 <davidbatonda7@gmail.com>
2024-10-01 23:11:49 +1000

    Add sols

--- Q1.py
+++ Q1.py
@@ -1,8 +1,8 @@
 from typing import List
+from algorithms.mergesort import mergesort
 
 def sort_the_students(score: List[List[int]], k) -> bool:
-	raise NotImplementedError()
-
+	return mergesort(score, key=lambda student_lst: -student_lst[k])
 
 if __name__ == "__main__":
 	pass
--- Q2.py
+++ Q2.py
@@ -1,5 +1,20 @@
+from typing import List
+from algorithms.mergesort import mergesort
+
 def sort_the_sentence(s: str) -> bool:
-	raise NotImplementedError()
+    split_str = s.split(" ")
+    new_lst: List[List[str]] = [] # to store [string, index]
+    for word_and_idx in split_str:
+          new_lst.append([word_and_idx[:-1], word_and_idx[-1]])
+
+    lst = mergesort(new_lst, lambda lst: lst[1])
+    
+    result: List[str] = []
+    for word, idx in lst:
+        result.append(word)
+    
+    return " ".join(result)
+	# raise NotImplementedError()
 
 
 if __name__ == "__main__":
